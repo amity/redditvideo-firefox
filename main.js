e352c0160e01817ee7f9a7dd151e8add51b53192
Sophie Amity Debs <sophie.a.debs@gmail.com>
2025-05-05 23:35:34 -0700

    first commit

---
+++ main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+function getLink(info, tab) {
+  console.log('its running');
+  fetch(info.linkUrl + '.json').then(resp =>
+    resp.json().then(pageJson => {
+      browser.tabs.executeScript(tab.id, {
+        frameId: info.frameId,
+        code: `window.prompt('Copy to clipboard: Cmd+C, Enter', "${pageJson[0].data.children[0].data.secure_media.reddit_video.fallback_url + '.gif'}");`,
+      })
+    }
+    )
+  )
+}
+
+browser.contextMenus.create({
+  id: "reddit-video-extractor",
+  title: "Get direct video link CMs",
+  contexts:["link"],  // ContextType
+  onclick: getLink // A callback function
+});
